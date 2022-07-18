fad8e9479c8c808a361ed5df8c38bee51f3d34b2
Boussad <faouzi.boussad@capgemini.com>
2022-07-18 14:10:15 +0200

    cleaning code

--- src/main/java/blockchainPackage/Main.java
+++ src/main/java/blockchainPackage/Main.java
@@ -1,25 +1,16 @@
 package blockchainPackage;
 
-
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.File;
-import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-
-
 
 public class Main {
 
 
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
 
 
         Blockchain blockchain = new Blockchain();
 
-       // BlockchainExplorer.printGenesisBlock(blockchain);
+        BlockchainExplorer.printGenesisBlock(blockchain);
 
         User firstUser = new User("JAJA",blockchain.getGenesisBlock().getListTransaction().get(0).getAmount());
         Block block1 = new Block(firstUser.getListTransaction().get(0));
@@ -33,12 +24,12 @@ public class Main {
 
         Block block2 = new Block(firstUser.send(20.0, thirdUser));
         blockchain.addBlock(block2);
-     //   BlockchainExplorer.printBlockchainDetails(blockchain);
-     //   BlockchainExplorer.printBlockDetails(0,blockchain);
+        BlockchainExplorer.printBlockchainDetails(blockchain);
+        BlockchainExplorer.printBlockDetails(0,blockchain);
 
         Wallet.printUserListTransactions("HAHA");
 
-       // BlockchainExplorer.printBlockDetails("e22888f2332bcc2baf79913b244678b809c1dbf645f8781e566093000853613f",blockchain);
+        BlockchainExplorer.printBlockDetails("e22888f2332bcc2baf79913b244678b809c1dbf645f8781e566093000853613f",blockchain);
 
     }
 }
